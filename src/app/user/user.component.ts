9a6ed21d48f48f70611d6d0bc661b0b21d271f07
Piyush Chand <piyushchand.pc@gmail.com>
2024-09-12 13:02:40 +0530

    output and emmitting data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,10 +8,17 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required:true}) id!:string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  // avatar= input.required<string>();
+  // name = input.required<string>();
+
 
    onUserClick(){
+    this.select.emit(this.id)
    }
 
    get imagePath(){
